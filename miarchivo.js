2c1283d3278dbd9ed3dbc8c601c630830d1fb7b3
darwinQSot <quispesotodaniel@gmail.com>
2023-03-04 13:31:28 -0500

    Uso de DOM y eventos

--- miarchivo.js
+++ miarchivo.js
@@ -12,40 +12,68 @@ const catalogo = [
   new Producto(1, 'Camisa', 12.90),
   new Producto(2, 'Pantalón', 20.99),
   new Producto(3, 'Zapatos', 30.49),
-  new Producto(4, 'Polera', 15.99)
+  new Producto(4, 'Polera', 15.99),
+  new Producto(5, 'Casaca', 40.99)
 ];
 
 //Uso de arrar con scope global
 let carrito = [];
 
-//limpiar el carrito
-const vaciarCarrito = () => {
-  carrito = [];
-  actualizarCarrito();
-}
+// Funcion para mostar productos en el HTML
+const mostrarProductos = () => {
+  console.log("en mostrar producto!!!");
+  const listaProductos = document.getElementById('lista-producto');
+  let indice = 0;
+  catalogo.forEach(producto => {
+    indice++;
+    console.log("en FOR EACH!!!");
+    const li = document.createElement('li');
+    const textoLi = document.createTextNode(producto.nombre + ' - $' + producto.precio.toFixed(2));
+    const cantidadInput = document.createElement("input");
+    cantidadInput.type = "number";
+    cantidadInput.className = "cantidad-producto";
+    cantidadInput.min = "1";
+    cantidadInput.value = "1";
+
+    const button = document.createElement('button');
+    button.textContent = 'Agregar al carrito';
+    button.onclick = function() {
+      var cantidad = this.previousSibling.value;
+      agregarAlCarrito(producto.id, cantidad);
+    };
+    li.appendChild(textoLi);
+    li.appendChild(cantidadInput);
+    li.appendChild(button);
+    listaProductos.appendChild(li);
+  });
+};
+
 
 // Función para actualizar la lista del carrito y el total, promedio, máximo, mínimo usando métodos de Math
 const actualizarCarrito = () => {
+  const listaCarrito = document.getElementById('lista-carrito');
   let total = 0;
-  let listaCarrito = '';
+  listaCarrito.innerHTML = '';
   carrito.forEach(item => {
-    listaCarrito += item.producto.nombre + ' x ' + item.cantidad + ' - $' + (item.producto.precio * item.cantidad).toFixed(2) + ' \n';
+    const li = document.createElement('li');
+    li.textContent = item.producto.nombre + ' x ' + item.cantidad + ' - $' + (item.producto.precio * item.cantidad).toFixed(2);
+    listaCarrito.appendChild(li);
     total += (item.producto.precio * item.cantidad);
   });
   if(carrito.length > 0) {
-    listaCarrito += 'Total: $' + total.toFixed(2)+'\n';
-    listaCarrito += 'Promedio: $' + (total / carrito.length).toFixed(2)+'\n';
-    listaCarrito += 'Máximo: $' + Math.max(...carrito.map(item => item.producto.precio * item.cantidad)).toFixed(2)+'\n';
-    listaCarrito += 'Mínimo. $' + Math.min(...carrito.map(item => item.producto.precio * item.cantidad)).toFixed(2)+'\n';
+    document.getElementById('total').textContent = 'Total: $' + total.toFixed(2);
+    document.getElementById('promedio').textContent = 'Promedio: $' + (total / carrito.length).toFixed(2);
+    document.getElementById('maximo').textContent = 'Máximo: $' + Math.max(...carrito.map(item => item.producto.precio * item.cantidad)).toFixed(2);
+    document.getElementById('minimo').textContent = 'Mínimo. $' + Math.min(...carrito.map(item => item.producto.precio * item.cantidad)).toFixed(2);
   }
-  return listaCarrito;
 }
+
 
 // Función para agregar un producto al carrito
 const agregarAlCarrito = (id, cantidad) => {
   const producto = catalogo.find(producto => producto.id === id);
   if (!producto) {
-    alert('Producto no encontrado en el catálogo');
+    console.error('Producto no encontrado en el catálogo');
     return;
   }
   const productoEnCarrito = carrito.find(item => item.producto.nombre === producto.nombre);
@@ -54,55 +82,38 @@ const agregarAlCarrito = (id, cantidad) => {
   } else {
     carrito.push({ producto, cantidad: parseInt(cantidad) });
   }
+  actualizarCarrito();
   console.log('Producto agregado al carrito:', producto.nombre);
   console.log('Carrito:', carrito);
 };
 
-let mostrarProductos = () => {
-  let listaProductos = ' Código:  |   Nombre:    | Precio: \n';
-  catalogo.forEach(producto => {
-    listaProductos += producto.id + '             |   '+ producto.nombre + '     |    $' + producto.precio.toFixed(2) + '\n';
-  });
-  return listaProductos;
-};
 
-// Función para filtrar los productos en el catálogo (funcionalidad a agregar con imputs HTML - próxima entrega)
+
+// Función para filtrar los productos en el catálogo
 const filtrarProductos = () => {
-  const productosBaratos = catalogo.filter(producto => producto.precio < 10);
-  let listaFiltrada = '';
+  const valor = document.getElementById("filtro").value;
+  console.log('filtrarProductos valor a filtrar: ' + valor);
+  const listaFiltrada = document.getElementById('lista-filtrada');
+  listaFiltrada.innerHTML = '';
+  const productosBaratos = catalogo.filter(producto => producto.precio < valor);
   productosBaratos.forEach(producto => {
-    listaFiltrada += producto.nombre + ' - $' + producto.precio.toFixed(2) + '\n';
+    const li = document.createElement('li');
+    li.textContent = producto.nombre + ' - $' + producto.precio.toFixed(2);
+    listaFiltrada.appendChild(li);
   });
 };
 
 
-alert("Bienvenido al carrito de compras! \n");
-//Inicio interacción
-let codigoProducto = -1;
-while(codigoProducto!=0){
-  codigoProducto = parseInt(prompt("Ingrese el código del producto a agregar: ingrese (0) para salir Ó (-1) para limpiar el carrito : \n"
- + mostrarProductos()
- + actualizarCarrito()));
-
-  console.log("Opcion: " + codigoProducto);
-  console.log("NUMBER? " + Number.isNaN(codigoProducto));
-
-  if(Number.isNaN(codigoProducto))
-    alert("Opción no valida!!!");
-  else{
-    codigoProducto = parseInt(codigoProducto);
-    if(codigoProducto === 0)
-    {
-      alert("Gracias por visitarnos, vuelva pronto!");
-    } 
-    else if(codigoProducto === -1){
-      vaciarCarrito();
-    }
-    else{
-      agregarAlCarrito(codigoProducto , 1);
-    }
-  }
+const vaciarCarrito = () => {
+  carrito = [];
+  document.getElementById('total').textContent = '';
+  document.getElementById('promedio').textContent = '';
+  document.getElementById('maximo').textContent = '';
+  document.getElementById('minimo').textContent = '';
+  actualizarCarrito();
 }
+// Mostrar los productos del catálogo
+mostrarProductos();
 
 
 
